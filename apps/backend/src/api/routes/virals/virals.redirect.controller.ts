a6da9e96911b7b22837416def68e059dbf0403b6
specialOne-coder <ferdiattivi@gmail.com>
2026-03-18 22:48:45 +0100

    fix redirection build issue

--- apps/backend/src/api/routes/virals/virals.redirect.controller.ts
+++ apps/backend/src/api/routes/virals/virals.redirect.controller.ts
@@ -1,15 +1,12 @@
 import { Controller, Get, Query } from '@nestjs/common';
-import { InjectRedis } from '@nestjs-modules/ioredis';
-import Redis from 'ioredis';
+import { ioRedis } from '@gitroom/nestjs-libraries/redis/redis.service';
 
 @Controller('/public/v1')
 export class ViralsRedirectController {
-  constructor(@InjectRedis() private readonly redis: Redis) {}
-
   @Get('/redirect-url')
   async getRedirectUrl(@Query('state') state: string) {
     if (!state) return { redirectUrl: null };
-    const redirectUrl = await this.redis.get(`redirect:${state}`);
+    const redirectUrl = await ioRedis.get(`redirect:${state}`);
     return { redirectUrl: redirectUrl ?? null };
   }
 }
